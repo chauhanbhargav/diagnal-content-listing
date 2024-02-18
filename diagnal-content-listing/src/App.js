1a48cde98e38003c70a586f45264c7a4cb6b777e
Bhargav Chauhan <bharagvchauhan777@gmail.com>
2024-02-18 21:14:52 +0530

    Fixed search and pagination issue, Also added description for each element and business logic

--- diagnal-content-listing/src/App.js
+++ diagnal-content-listing/src/App.js
@@ -1,27 +1,32 @@
-import { lazy, useEffect, useState } from 'react';
+import React, { lazy, useCallback, useEffect, useState } from 'react';
 import './App.css';
 
+//  Lazy Imports
 const Header = lazy(() => import("./Header"));
 const Content = lazy(() => import("./Content"));
 
 function App() {
 
+    //  Local state to maintain response
     const [data, setData] = useState([]);
     const [page, setPage] = useState(1);
     const [response, setResponse] = useState(null);
 
+    //  Get Initial content
     useEffect(() => {
         getContents();
     }, []);
 
+    //  On scroll fire callback event to get pagination
     useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, []);
+        document.addEventListener("scroll", handleScroll, { passive: true });
+        return () => document.removeEventListener("scroll", handleScroll);
+    }, [response]);
 
-    const getContents = async (currentPage) => {
+    //  Get contents
+    const getContents = async (cPage) => {
         try {
-            const res = await fetch(`${process.env.REACT_APP_BASE_API_URL}data/page${currentPage || page}.json`)
+            const res = await fetch(`${process.env.REACT_APP_BASE_API_URL}data/page${cPage || page}.json`)
             const json = await res?.json();
             if (json) {
                 const allData = page > 1 ? json?.page?.['content-items']?.content : [...data, ...json?.page?.['content-items']?.content];
@@ -33,31 +38,54 @@ function App() {
         }
     }
 
-    function handleScroll() {
+    //  Onscroll Callback - Call only dependency change
+    const handleScroll = useCallback(() => {
+        onScroll()
+    }, [response]);
+
+
+    function onScroll() {
         if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
             const totalPageAccessible = Math.round(Number(response?.page?.['total-content-items']) / 20);
             if (page < totalPageAccessible) {
-                setPage(p => p + 1)
-                getContents()
+                const newPage = page + 1;
+                setPage(newPage)
+                getContents(newPage)
             }
         }
     }
 
+    //  Client side search
     const handleClientSideSearch = (searchText) => {
         if (!searchText) {
             getContents(1)
-        } else {
-            const searchData = data?.filter((content) => content.name.search(searchText));
+        }
+        if (searchText) {
+            const searchData = data?.filter((item) => {
+                const input = searchText.trim().toLowerCase();
+                return ['name'].some(prop => item[prop].toLowerCase().includes(input));
+            });
             setData(searchData);
         }
     }
 
     return (
         <div className="App">
-            <Header title={response?.page?.title} handleSearch={handleClientSideSearch} closeSearch={() => getContents(1)} />
+            {/* Header Component wth label and search */}
+            <Header
+                title={response?.page?.title || "-"}
+                handleSearch={(e) => handleClientSideSearch(e)}
+                closeSearch={() => getContents(1)}
+            />
+
+            {/* Body content */}
             <div className='content-container'>
                 {data?.map((content, i) => (
-                    <Content content={content} key={i} />
+                    <Content
+                        key={i}
+                        index={i}
+                        content={content}
+                    />
                 ))}
             </div>
         </div>
--- diagnal-content-listing/src/Content.jsx
+++ diagnal-content-listing/src/Content.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Content = ({ content, key }) => {
+const Content = ({ content, index }) => {
     return (
-        <div key={key} className='img-content-item'>
+        <div key={`content-${index}`} className='img-content-item'>
             <img src={`${process.env.REACT_APP_BASE_API_URL}images/${content?.['poster-image']}`} alt="" />
             <p>{content.name}</p>
         </div>
